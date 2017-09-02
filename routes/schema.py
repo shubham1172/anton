eb2aef9d6f823b3722bb9855c3cca1b26fe7ad05
shubham1172 <shubhamsharma1172@gmail.com>
2017-09-02 13:54:02 +0530

    Schema updated

--- routes/schema.py
+++ routes/schema.py
@@ -4,5 +4,10 @@ from . import routes
 
 @routes.route('/schemas')
 def schemas():
-    connstring = current_app.config["connstring"]
-    return render_template('schema.html', host=connstring["host"], port=connstring["port"])
+    connstring = current_app.config["connstring"].to_dict()
+    #Create a template dict
+    connstring.pop("password")
+    curr = current_app.config["conn"].cursor()
+    curr.execute('SELECT schema_name FROM information_schema.schemata')
+    rows = curr.fetchall()
+    return render_template('schema.html', template=connstring, schemas=rows)
