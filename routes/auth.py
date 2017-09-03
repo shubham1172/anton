3e546a82b89358736cdeff94520bafe78d3be7ae
shubham1172 <shubhamsharma1172@gmail.com>
2017-09-03 17:43:01 +0530

    before_first_req added

--- routes/auth.py
+++ routes/auth.py
@@ -7,7 +7,7 @@ def index():
     #If logged in, redirect to schemas
     if "conn" in current_app.config.keys():
         return redirect('/model')
-    return current_app.send_static_file('index.html')
+    return current_app.send_static_file('home.html')
 
 @routes.route('/login', methods=['POST'])
 def login():
