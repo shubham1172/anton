92635ec8159e3a4d97d75807f522014db19119ae
shubham1172 <shubhamsharma1172@gmail.com>
2017-09-10 21:44:27 +0530

    Table updated

--- routes/model.py
+++ routes/model.py
@@ -6,6 +6,11 @@ from . import routes
 def model():
     connstring = current_app.config["connstring"]
     curr = current_app.config["conn"].cursor()
-    curr.execute('SELECT schema_name FROM information_schema.schemata')
+    query = """SELECT schema_name
+                FROM information_schema.schemata"""
+    try:
+        curr.execute(query)
+    except:
+        abort(500)
     rows = curr.fetchall()
     return render_template('model.html', template=connstring, schemas=rows)
--- routes/table.py
+++ routes/table.py
@@ -1,6 +1,21 @@
-from flask import request
+from flask import request, current_app, render_template, abort
 from . import routes
 
 @routes.route('/model/<schema>/<table>')
 def table(schema,table):
-    return "Table view"
+    connstring = current_app.config["connstring"]
+    curr = current_app.config["conn"].cursor()
+    query = """SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name LIKE '{}'
+                    AND table_schema LIKE '{}'""".format(table, schema)
+    try:
+        curr.execute(query)
+    except:
+        abort(500)
+    message = ""
+    rows = curr.fetchall()
+    if len(rows)==0:
+        message = "No columns found. Table name invalid/Table is empty"
+    return render_template('table.html', message=message,
+        schema=schema, table=table, template=connstring, columns=rows)
