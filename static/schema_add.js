f9e952a6829764dc3378e73ea0b54f1384841de4
ANUJA NEGI <anujanegi1@gmail.com>
2017-09-26 20:44:24 +0530

    logging create-shema response

--- static/schema_add.js
+++ static/schema_add.js
@@ -1,11 +1,16 @@
-document.onload = function() {
+window.onload = function() {
   var create_schema = document.getElementById('create_schema');
   create_schema.onclick = function(){
-    console.log('hi');
     var request = new XMLHttpRequest();
     var schema = document.getElementById('schema_name').value;
     request.open('POST', '/api/create-schema', true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({schema: schema}));
-  };
-};
+    var message = document.getElementById('message');
+    if(request.readystate = XMLHttpRequest.DONE){
+      console.log(JSON.stringify(request.responseText));
+      console.log(JSON.stringify(request.responseCode));}
+    //console.log(msg);
+    message.innerHTML='message';
+  }
+}
