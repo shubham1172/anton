ff0a2fb4620af6fdb0767dff6bc7de5f29bd6ca0
shubham1172 <shubhamsharma1172@gmail.com>
2017-09-26 18:57:39 +0530

    updated status codes

--- api/sender.py
+++ api/sender.py
@@ -4,13 +4,13 @@ from flask import jsonify
 def OK(data, args = None):
     if args:
         return jsonify(code=200, data=data, args=args)
-    return jsonify(code=200, data=data)
+    return jsonify(code=200, data=data), 200
 
 def BadRequest(message="incorrect parameters"):
-    return jsonify(code=400, message=message)
+    return jsonify(code=400, message=message), 400
 
 def Forbidden(message):
-    return jsonify(code=403, message=message)
+    return jsonify(code=403, message=message), 403
 
 def Error(message):
-    return jsonify(code=500, message=message)
+    return jsonify(code=500, message=message), 500
