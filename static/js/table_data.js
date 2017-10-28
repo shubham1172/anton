3ce9d7c085c450bd5cb3e5ee97ac786473afa30d
ANUJA NEGI <anujanegi1@gmail.com>
2017-10-28 17:51:16 +0530

    Input box to alter on dblclick added

--- static/js/table_data.js
+++ static/js/table_data.js
@@ -1,22 +1,43 @@
-window.onload = function() {
-
-  function getHTML(column_name, row, length) {
-    var html="<tr>";
-    for (var i=0 ; i<length ; i++)
-    {
-      html+="<td><b>"+column_name[i]+"</b></td>";
+function getHTML(column_name, row, length) {
+  var html="<tr>";
+  for (var i=0 ; i<length ; i++)
+  {
+    html+="<td><b>"+column_name[i]+"</b></td>";
+  }
+  html+="</tr>";
+  for (var i=0 ; i<row.length ; i++)
+  {
+    html+="<tr>";
+    for(var j=0 ; j<length ; j++) {
+      html+="<td id="+row[i][j]+" ondblclick='addInput(this);'>"+row[i][j]+"</td>";
     }
     html+="</tr>";
-    for (var i=0 ; i<row.length ; i++)
-    {
-      html+="<tr>";
-      for(var j=0 ; j<length ; j++) {
-        html+="<td>"+row[i][j]+"</td>";
-      }
-      html+="</tr>";
-    }
-    return html;
   }
+  return html;
+}
+
+function closeInput(elm) {
+    var td = elm.parentNode;
+    var value = elm.value;
+    td.removeChild(elm);
+    //updateTable(value, id);
+    td.innerHTML = value;
+}
+
+function addInput(elm) {
+    if (elm.getElementsByTagName('input').length > 0) return;
+    var value = elm.innerHTML;
+    elm.innerHTML = '';
+    var input = document.createElement('input');
+    input.setAttribute('id', elm.id);
+    input.setAttribute('type', 'text');
+    input.setAttribute('value', value);
+    input.setAttribute('onBlur', 'closeInput(this)');
+    elm.appendChild(input);
+    input.focus();
+}
+
+window.onload = function() {
 
   var url=window.location.href;
   var arr=url.split("/");
@@ -36,7 +57,7 @@ window.onload = function() {
         var length = column_name.length;
         document.getElementById('table_data').innerHTML = getHTML(column_name, row, length);
       }
-
     }
   }
+
 }
