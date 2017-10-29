2bb55ef7810b1f1c2607ebc4c4741d31a342e320
ANUJA NEGI <anujanegi1@gmail.com>
2017-10-29 17:00:04 +0530

    ALter data for strings added

--- static/js/table_data.js
+++ static/js/table_data.js
@@ -9,19 +9,51 @@ function getHTML(column_name, row, length) {
   {
     html+="<tr>";
     for(var j=0 ; j<length ; j++) {
-      html+="<td id="+row[i][j]+" ondblclick='addInput(this);'>"+row[i][j]+"</td>";
+      html+="<td id="+row[i][j]+" class=\""+column_name[j]+"\" ondblclick='addInput(this);'>"+row[i][j]+"</td>";
     }
     html+="</tr>";
   }
   return html;
 }
 
+function updateTable(value, id, name) {
+  var url=window.location.href;
+  var arr=url.split("/");
+  var sql_query;
+  if(id=="null") {
+    sql_query = "UPDATE "+arr[4]+"."+arr[5]+" SET "+name+"='"+value+"' WHERE "+name+"='';";
+  }
+  else {
+    sql_query = "UPDATE "+arr[4]+"."+arr[5]+" SET "+name+"='"+value+"' WHERE "+name+"='"+id+"';";
+  }
+  console.log(sql_query);
+  var request = new XMLHttpRequest();
+  request.open('POST', '/api/raw-sql', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({query: sql_query}));
+  request.onload = function(){
+    if(request.readystate = XMLHttpRequest.DONE){
+      var message = document.getElementById("message");
+      var msg = JSON.parse(request.responseText);
+      if (msg.message=="no results to fetch"){
+      }
+      else{
+        message.innerHTML=msg.message;
+      }
+    }
+  }
+}
+
 function closeInput(elm) {
+  console.log(elm);
     var td = elm.parentNode;
     var value = elm.value;
+    var name = elm.name;
+    var id = elm.id;
     td.removeChild(elm);
-    //updateTable(value, id);
+    updateTable(value, id, name);
     td.innerHTML = value;
+    console.log(elm);
 }
 
 function addInput(elm) {
@@ -31,6 +63,7 @@ function addInput(elm) {
     var input = document.createElement('input');
     input.setAttribute('id', elm.id);
     input.setAttribute('type', 'text');
+    input.setAttribute('name', elm.className);
     input.setAttribute('value', value);
     input.setAttribute('onBlur', 'closeInput(this)');
     elm.appendChild(input);
@@ -59,5 +92,4 @@ window.onload = function() {
       }
     }
   }
-
 }
