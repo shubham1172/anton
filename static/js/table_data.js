187887ebadde8c31bc1cdbe85d6385562b4433ca
ANUJA NEGI <anujanegi1@gmail.com>
2017-10-29 18:24:01 +0530

    Alter table data updated

--- static/js/table_data.js
+++ static/js/table_data.js
@@ -1,4 +1,4 @@
-function getHTML(column_name, row, length) {
+function getHTML(column_name, type, row, length) {
   var html="<tr>";
   for (var i=0 ; i<length ; i++)
   {
@@ -9,22 +9,23 @@ function getHTML(column_name, row, length) {
   {
     html+="<tr>";
     for(var j=0 ; j<length ; j++) {
-      html+="<td id="+row[i][j]+" class=\""+column_name[j]+"\" ondblclick='addInput(this);'>"+row[i][j]+"</td>";
+      html+="<td id="+row[i][j]+" class=\""+column_name[j]+"\" headers =\""+type[j]+"\" ondblclick='addInput(this);'>"+row[i][j]+"</td>";
     }
     html+="</tr>";
   }
   return html;
 }
 
-function updateTable(value, id, name) {
+function updateTable(value, id, name, type) {
+  //console.log(type);
   var url=window.location.href;
   var arr=url.split("/");
   var sql_query;
-  if(id=="null") {
-    sql_query = "UPDATE "+arr[4]+"."+arr[5]+" SET "+name+"='"+value+"' WHERE "+name+"='';";
+  if (type == "character varying" || type == "text") {
+    sql_query = "UPDATE "+arr[4]+"."+arr[5]+" SET "+name+"='"+value+"' WHERE "+name+"='"+id+"';";
   }
   else {
-    sql_query = "UPDATE "+arr[4]+"."+arr[5]+" SET "+name+"='"+value+"' WHERE "+name+"='"+id+"';";
+    sql_query = "UPDATE "+arr[4]+"."+arr[5]+" SET "+name+"="+value+" WHERE "+name+"="+id+";";
   }
   console.log(sql_query);
   var request = new XMLHttpRequest();
@@ -45,18 +46,20 @@ function updateTable(value, id, name) {
 }
 
 function closeInput(elm) {
-  console.log(elm);
+    //console.log(elm);
     var td = elm.parentNode;
     var value = elm.value;
     var name = elm.name;
     var id = elm.id;
+    var type = elm.className;
     td.removeChild(elm);
-    updateTable(value, id, name);
+    updateTable(value, id, name, type);
     td.innerHTML = value;
-    console.log(elm);
+    //console.log(elm);
 }
 
 function addInput(elm) {
+    //console.log(elm);
     if (elm.getElementsByTagName('input').length > 0) return;
     var value = elm.innerHTML;
     elm.innerHTML = '';
@@ -64,6 +67,7 @@ function addInput(elm) {
     input.setAttribute('id', elm.id);
     input.setAttribute('type', 'text');
     input.setAttribute('name', elm.className);
+    input.setAttribute('class', elm.headers);
     input.setAttribute('value', value);
     input.setAttribute('onBlur', 'closeInput(this)');
     elm.appendChild(input);
@@ -86,9 +90,10 @@ window.onload = function() {
       }
       else {
         var column_name = response.data.headers;
+        var type = response.data.types;
         var row = response.data.values;
         var length = column_name.length;
-        document.getElementById('table_data').innerHTML = getHTML(column_name, row, length);
+        document.getElementById('table_data').innerHTML = getHTML(column_name, type, row, length);
       }
     }
   }
