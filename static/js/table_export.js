cc646a099ec4b385934cd8fa6d3897c78ea6e169
ANUJA NEGI <anujanegi1@gmail.com>
2017-11-02 22:31:24 +0530

    Export fully functional

--- static/js/table_export.js
+++ static/js/table_export.js
@@ -1,20 +1,22 @@
-function exportTable() {
-  var textToSave = "anuja";
-  var filename = 'file.json';
+function exportToFile(data,table) {
+  var textToSave = data;
+  var filename = table+".json";
   var blob = new Blob([textToSave], {type: "text/plain;charset=utf-8"});
   saveAs(blob, filename);
 }
-/*window.onload = function () {
-  var table_export = document.getElementById('export');
 
-  function exportTable(table, data)
-  {
-
+function exportTable(that) {
+  var table_name = that.id;
+  var url=window.location.href;
+  var arr=url.split("/");
+  var schema_name = arr[4];
+  var request = new XMLHttpRequest();
+  request.open('POST', '/api/export-table', true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({table: table_name,schema: schema_name}));
+  request.onload = function(){
+    if(request.readystate = XMLHttpRequest.DONE){
+      exportToFile(request.responseText,table_name);
+    }
   }
-  table_export.onclick = function () {
-    var textToSave = "anuja";
-    var filename = 'file.json';
-    var blob = new Blob([textToSave], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, filename);
-  }
-}*/
+}
