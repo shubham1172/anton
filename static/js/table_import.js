204bba10978977ab409a6a4274d695243eba90d2
ANUJA NEGI <anujanegi1@gmail.com>
2017-11-03 01:06:32 +0530

    Updated

--- static/js/table_import.js
+++ static/js/table_import.js
@@ -1,41 +1,28 @@
-function import_table(data) {
-  var table_name = document.getElementById('table_name').value;
-  console.log(table_name);
-  var url=window.location.href;
-  var arr=url.split("/");
-  var schema_name = arr[4];
-  console.log(schema_name);
-  var request = new XMLHttpRequest();
-  request.open('POST', '/api/import-table', true);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send(JSON.stringify({table: table_name,schema: schema_name,data: data}));
-  request.onload = function(){
-    if(request.readystate = XMLHttpRequest.DONE){
-      console.log('done');
-    }
-  }
-}
-
 window.onload = function () {
-  var files = document.getElementById('table_data').files;
   var message=document.getElementById("message");
 
   document.getElementById('import').onclick = function() {
-      var formatted_data;
-      if (files.length <= 0)
-        message.innerHTML='Kindly select a file.';
-      else if (files[0].type!="application/json")
-        message.innerHTML='Select a JSON file.';
-      else {
+        var formatted_data, data, files;
+        files = document.getElementById('table_data').files;
         var read_data = new FileReader();
         read_data.onload = function(file_data) {
-        var data = JSON.parse(file_data.target.result);
-        formatted_data = JSON.stringify(data, null, 2);
-        console.log(formatted_data);
+          data = JSON.parse(file_data.target.result);
+          formatted_data = JSON.stringify(data, null, 2);
+          console.log(formatted_data);
+          var table_name = document.getElementById('table_name').value;
+          var url=window.location.href;
+          var arr=url.split("/");
+          var schema_name = arr[4];
+          var request = new XMLHttpRequest();
+          request.open('POST', '/api/import-table', true);
+          request.setRequestHeader('Content-Type', 'application/json');
+          request.send(JSON.stringify({table: table_name,schema: schema_name,data: formatted_data}));
+          request.onload = function(){
+            if(request.readystate = XMLHttpRequest.DONE){
+              console.log('done');
+            }
+          }
         }
         read_data.readAsText(files.item(0));
-        import_table(formatted_data);
-      }
-    }
-
+}
 }
