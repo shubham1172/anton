29e83ba9fee990c1ec7b80024bab22ea781653ba
ANUJA NEGI <anujanegi1@gmail.com>
2017-11-03 01:24:29 +0530

    Importing fixed

--- static/js/table_import.js
+++ static/js/table_import.js
@@ -4,10 +4,14 @@ window.onload = function () {
   document.getElementById('import').onclick = function() {
         var formatted_data, data, files;
         files = document.getElementById('table_data').files;
+        if (files.length <= 0)
+        message.innerHTML='Kindly select a file.';
+      else if (files[0].type!="application/json")
+        message.innerHTML='Select a JSON file.';
+      else {
         var read_data = new FileReader();
         read_data.onload = function(file_data) {
           data = JSON.parse(file_data.target.result);
-          formatted_data = JSON.stringify(data, null, 2);
           console.log(formatted_data);
           var table_name = document.getElementById('table_name').value;
           var url=window.location.href;
@@ -16,13 +20,14 @@ window.onload = function () {
           var request = new XMLHttpRequest();
           request.open('POST', '/api/import-table', true);
           request.setRequestHeader('Content-Type', 'application/json');
-          request.send(JSON.stringify({table: table_name,schema: schema_name,data: formatted_data}));
+          request.send(JSON.stringify({table: table_name,schema: schema_name,data: data}));
           request.onload = function(){
-            if(request.readystate = XMLHttpRequest.DONE){
-              console.log('done');
+            if(request.responseText.code = 200){
+              window.location = "http://localhost/model/"+arr[4];
             }
           }
         }
         read_data.readAsText(files.item(0));
-}
+      }
+    }
 }
